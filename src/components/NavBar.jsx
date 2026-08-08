58c9aa0dce7b27414ebee9529c4f3cb1160f73bb
Michael Orji <xlence1624@gmail.com>
2026-08-08 15:43:24 +0100

    blogpost landing page created
    Signed-off-by: Michael Orji <xlence1624@gmail.com>

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+
+const NavBar = () => {
+const navigate  = useNavigate()
+
+
+  return (
+    <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32  ">
+      <div className="flex items-center gap-2 cursor-pointer" onClick={()=>navigate("/")}>
+  <img src={assets.eyilslogo} alt="Logo" className="w-8 border border-primary rounded-full sm:w-44 "   />
+  <h1 className="text-2xl text-gray-700 font-bold">Eyil's Pen</h1>
+      </div>
+    
+      <button  onClick={()=>navigate("/admin")} className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5 ">Login
+        <img src={assets.arrow} className="w-3" alt="arrow" />
+      </button>
+
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../components/NavBar.jsx'
+import Header from '../components/Header.jsx'
+import Bloglist from '../components/Bloglist.jsx'
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <Header/>
+      <Bloglist/>
+    </>
+  )
+}
+
+export default Home
