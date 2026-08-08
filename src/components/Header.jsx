24968287e229d9543892e30b253a35ba4b7bd2ca
Michael Orji <xlence1624@gmail.com>
2026-08-08 16:12:43 +0100

    mt reduced in header
    Signed-off-by: Michael Orji <xlence1624@gmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Header = () => {
   return (
     <div className='mx-8 sm:mx-16 xl:mx-24 relative flex items-center flex-col  justify-center mt-18 text-center'>
-      <div clasName=" mt-20 mb-8">
+      <div clasName=" mt-10 mb-8">
 <div className="inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border border-primary/40 bg-primary/10 rounded-full text-sm text-primary">
   <p>Welcome to the Eyils Universe</p>
   <img src={assets.star_icon} alt="Star Icon" className="w-2.5"/>
