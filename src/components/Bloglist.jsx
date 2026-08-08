02dbaadaeb39898a12973e8297da064bb27db4ad
Michael Orji <xlence1624@gmail.com>
2026-08-08 16:40:41 +0100

    category layout fixed
    Signed-off-by: Michael Orji <xlence1624@gmail.com>

--- src/components/Bloglist.jsx
+++ src/components/Bloglist.jsx
@@ -11,12 +11,12 @@ const Bloglist = () => {
 {
   blogCategories.map(
     (item) => (
-      <div key={item} className="relative ">
+      <div key={item} className="relative text-xs sm:text-sm ">
         <button
         onClick={()=>setMenu(item)}
         className={`cursor-pointer text-gray-500 ${menu === item && "text-white px-4 pt-0.5"}`}>
           {item}
-          {menu === item && (    <motion.div layoutId='underline' transition={{type: 'sping', stiffness: 500, damping: 30}}className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full">
+          {menu === item && (    <motion.div layoutId='underline' transition={{type: 'sping', stiffness: 500, damping: 30}}className="absolute left-0 right-0 -top-1 sm:-top-0.5 h-7 -z-1 bg-primary rounded-full">
 
           </motion.div>)}
       
