9484833391d23380c197bdb896ab021f87656a2e
Orji Michael <xlence1624@gmail.com>
2026-08-26 09:52:07 +0100

    update

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,28 +3,32 @@ import { useNavigate } from "react-router-dom";
 
 export default function ArticleCard({ article }) {
   const navigate = useNavigate();
-
+  const {title, description, image, author, date, _id, category, readTime} = article
   // If the article prop hasn't loaded yet, prevent a crash
   if (!article) return null;
 
   return (
     <article
       className="article-card cursor-pointer"
-      onClick={() => navigate(`/blog/${article._id}`)}
+      onClick={() => navigate(`/blog/${_id}`)}
     >
       <div className="card-image">
-         <img src={article.image} alt={article.title || "Article Image"} />
+         <img src={image} alt={title || "Article Image"} />
       </div>
 
       <div className="card-body">
         <span className="eyebrow">
-          {article.category}
+          {category}
         </span>
 
-        <h3>{article.title}</h3>
+        <h3>{title}</h3>
+            
+     
+        <p className="mb-3 text-xs text-gray-500" dangerouslySetInnerHTML={{ __html: description.slice(0,100) }} />
+    
 
         <div className="card-footer">
-          <span>{article.readTime}</span>
+          <span>{readTime}</span>
           <span className="arrow">→</span>
         </div>
       </div>
