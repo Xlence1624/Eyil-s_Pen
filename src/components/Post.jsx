d6f99dd16e8b7b62f4b4a8f21540a89ddb28881a
Orji Michael <xlence1624@gmail.com>
2026-09-04 16:17:24 +0100

    update

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,11 +34,11 @@ const Post = () => {
                 </div>
                 <div className="flex items-center gap-5 flex-wrap">
                     <div className="flex-1 flex flex-col gap-1 w-32">
-                        <label className="text-base font-medium" htmlFor="product-price">Product Price</label>
-                        <input id="product-price" type="number" placeholder="0" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
+                        <label className="text-base font-medium" htmlFor="product-price">Date</label>
+                        <input id="product-price" type="date" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
                     </div>
                     <div className="flex-1 flex flex-col gap-1 w-32">
-                        <label className="text-base font-medium" htmlFor="offer-price">Offer Price</label>
+                        <label className="text-base font-medium" htmlFor="offer-price">Read time</label>
                         <input id="offer-price" type="number" placeholder="0" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
                     </div>
                 </div>
