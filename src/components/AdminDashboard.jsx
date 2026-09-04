90a3ec31c7ffc9622063060e457e9563be4aba25
Orji Michael <xlence1624@gmail.com>
2026-09-04 16:33:06 +0100

    update

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useState } from 'react';
 import AdminHeader from './AdminHeader';
-import { useNavigate } from 'react-router-dom';
+
 import Post from  "./Post.jsx"
 import PostOverview from "./PostOverview.jsx"
 const AdminDashboard = () => {
-
+const [activePage, setActivePage] = useState('dashboard');
     const dashboardicon = (
         <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" strokeLinejoin="round" strokeWidth="2" d="M4 5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V5Zm16 14a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2ZM4 13a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6Zm16-2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v6Z" />
@@ -29,7 +29,7 @@ const AdminDashboard = () => {
         { id: "overview", name: "Posts Analytics",  icon: overviewicon, component: <PostOverview /> },
         { id: "chat", name: "Comments approval", icon: chaticon, component: <PostOverview /> },
     ];
-const [activePage, setActivePage] = useState('dashboard');
+
   const currentPage = sidebarLinks.find(item => item.id === activePage);
 
 
