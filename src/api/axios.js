d9c97df5167b4ad8df2b411882afc1b6847f2316
Orji Michael <xlence1624@gmail.com>
2026-09-05 22:10:10 +0100

    axiox fixed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api" || "https://eyil-s-pen.vercel.app/api",
+  baseURL:  "https://eyil-s-pen.vercel.app/api" 
 });
 
 api.interceptors.request.use((config) => {
