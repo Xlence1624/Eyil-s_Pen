d5b526fa6e0d5a8efb0732229607ccd5f0987500
Orji Michael <86269930+Xlence1624@users.noreply.github.com>
2026-09-06 09:57:51 +0100

    Change API base URL to localhost for development

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://eyil-s-pen-yn3r.vercel.app/api",
+  baseURL: "https://localhost:3000/api",
 });
 
 api.interceptors.request.use((config) => {
