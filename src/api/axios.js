7c785106bbb94810a81d884c8bc23937f33634ac
Orji Michael <86269930+Xlence1624@users.noreply.github.com>
2026-09-06 14:25:29 +0100

    Fix baseURL in axios API configuration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://xlence1624.github.io/herblogg//api",
+  baseURL: "https://xlence1624.github.io/herblogg/api",
 });
 
 api.interceptors.request.use((config) => {
