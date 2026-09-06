950e787bc2b0729baa634a3cb1cd7f22f616bb81
Orji Michael <86269930+Xlence1624@users.noreply.github.com>
2026-09-06 14:25:14 +0100

    Change API base URL to production endpoint

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://xlence1624.github.io/herblogg//api",
 });
 
 api.interceptors.request.use((config) => {
