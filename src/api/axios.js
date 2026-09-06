14f50336becccc9aabc76dc5520fd9459d751d94
Orji Michael <86269930+Xlence1624@users.noreply.github.com>
2026-09-06 16:14:15 +0100

    Update API base URL to Vercel deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://xlence1624.github.io/herblogg/api",
+  baseURL: "https://herblogg.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
