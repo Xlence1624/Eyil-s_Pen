de299284fbaac5b0b4ca138403db6ec36e073007
Orji Michael <xlence1624@gmail.com>
2026-09-09 09:20:49 +0100

    updated

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -38,9 +38,7 @@ Purpose, career, productivity and making impact.       </h1>
 
             <div className="gold-line" />
 
-            <p>
-          ME!!! …one who communicates better through writing and it is also my escape from anything, everything! Ah! Yes! I do ‘non-fictional fiction’, if there’s anything of sort. No one has a 100% ability to reach out to the world but then, we impact our individual spaces to the degree that we can. … reaching out to a few who in turn will reach out to others. This in mind, we are assured of a ‘close to perfect’ world. I will really appreciate feedbacks/comments.I love us all 💕💕
-            </p>
+         
 
             <a
              onClick={() => navigate(`/everything`)} 
