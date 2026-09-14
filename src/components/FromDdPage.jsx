0eae020f4a0eeddfd9d26ac65911795038b7bba0
Orji Michael <xlence1624@gmail.com>
2026-09-14 12:25:43 +0100

    fix

--- src/components/FromDdPage.jsx
+++ src/components/FromDdPage.jsx
@@ -360,7 +360,7 @@ const handleCopyLink = async () => {
           .replace(/\s+/g, "-");
 
         return (
-          <h2 id={id} {...props} className="hidden">
+          <h2 id={id} {...props} >
           {children}
           </h2>
         );
